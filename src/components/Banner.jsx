9f9f842df0b5bad711d73af90f01ccd606d39d9b
Aakashrajan1023 <aakash10203@gmail.com>
2026-02-17 11:09:37 +0530

    Updated project

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -13,20 +13,68 @@ import "swiper/css";
 import { Autoplay } from "swiper/modules";
 import BlurText from "./BlurText";
 
-const Banner = ({ handleClick }) => {
+const Banner = () => {
+  const slider = [
+    {
+      title: "Premium Cement Supply for ",
+      subtitle: " Stronger Foundations",
+      desc: "Delivering high-quality cement solutions for residential, commercial, and infrastructure projects with guaranteed strength, durability, and reliability.",
+      img: "/images/hero_test.jpeg",
+      path_1: "/contact",
+      path_2: "/products/cement",
+    },
+     {
+      title: "Premium Paints for Perfect Finishes",
+      subtitle: "Color Your World with Confidence",
+      desc: "Color Your World with Confidence",
+      img: "/images/hero_2.png",
+      path_1: "/contact",
+      path_2: "/products/paint",
+    },
+     {
+      title: "Strength You Can Build On",
+      subtitle: "Forging the Future with Quality Steel",
+      desc: "Supplying premium-grade steel products engineered for durability, structural integrity, and long-lasting performance in every project.",
+      img: "/images/hero_3.png",
+      path_1: "/contact",
+      path_2: "/products/steel",
+    },
+     {
+      title: "Strong Foundations Start with Quality Aggregates",
+      subtitle: "Building Better Infrastructure, From the Ground Up",
+      desc: "Delivering durable construction aggregates that enhance structural integrity and long-term performance.",
+      img: "/images/hero_4.png",
+      path_1: "/contact",
+      path_2: "/products/aggregates",
+    },
+     {
+      title: "Strong Bricks. Solid Foundations.",
+      subtitle: "Building the Future, Block by Block",
+      desc: "Supplying high-quality bricks and concrete blocks designed for durability, structural strength, and long-lasting construction performance.",
+      img: "/images/hero_5.png",
+      path_1: "/contact",
+      path_2: "/products/bricks",
+    },
+     {
+      title: "Smart Electrical & Plumbing Solutions",
+      subtitle: "Powering and Connecting Every Structure",
+      desc: "Supplying reliable electrical and plumbing materials designed for safety, efficiency, and long-lasting performance in every project.",
+      img: "/images/hero_6.png",
+      path_1: "/contact",
+      path_2: "/products/electrical",
+    },
+     {
+      title: "Essential Hardware for Every Build",
+      subtitle: "Built with Precision. Backed by Quality.",
+      desc: "Supplying high-quality hardware and construction consumables designed for durability, efficiency, and dependable performance on every project.",
+      img: "/images/hero_7.png",
+      path_1: "/contact",
+      path_2: "/products/hardware",
+    },
+  ];
+
   return (
     <section className="relative h-screen overflow-hidden">
-      {/* Background Video */}
-      {/* <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        preload="auto"
-        className="absolute inset-0 w-full h-full object-cover z-0"
-      >
-        <source src="/videos/hero_bg.mp4" type="video/mp4" />
-      </video> */}
       <Swiper
         modules={[Autoplay]}
         spaceBetween={20}
@@ -36,149 +84,70 @@ const Banner = ({ handleClick }) => {
         loop
         className="w-full h-full"
       >
-        <SwiperSlide>
-          <img
-            src="/images/hero_test.jpeg"
-            alt=""
-            className="absolute inset-0 w-full h-full object-cover z-0"
-          />
+        {slider.map((item, idx) => (
+          <SwiperSlide key={idx}>
+            <img
+              src={item.img}
+              alt=""
+              className="absolute inset-0 w-full h-full object-cover z-0"
+            />
 
-          <div className="absolute inset-0 bg-linear-to-t 
-                from-black/40 to-black/20"></div>
+            <div
+              className="absolute inset-0 bg-linear-to-t 
+                from-black/40 to-black/20"
+            ></div>
 
-          {/* Dark Overlay */}
-          {/* <div className="absolute inset-0 bg-black/40 z-10"></div> */}
+            <div className="relative z-20 flex flex-col items-center justify-center h-full text-white text-center px-4">
+              <div className="mb-3 md:mb-5">
+                <div className="flex justify-center">
+                  <BlurText
+                    text={item.title}
+                    delay={70}
+                    animateBy="words"
+                    direction="top"
+                    className="text-3xl md:text-5xl font-bold "
+                  />
+                </div>
 
-          {/* Content */}
-          <div className="relative z-20 flex flex-col items-center justify-center h-full text-white text-center px-4">
-            <div className="mb-3 md:mb-5">
-              <div className="flex justify-center">
-                <BlurText
-                  text="Your Trusted "
-                  delay={70}
-                  animateBy="words"
-                  direction="top"
-                  className="text-3xl md:text-5xl font-bold "
-                />
+                <div className="mt-2 md:mt-4 flex justify-center gap-2 flex-wrap">
+                  <SplitText
+                    text={item.subtitle}
+                    delay={40}
+                    animateBy="words"
+                    direction="top"
+                    className="text-3xl md:text-5xl font-bold text-[#125997]"
+                  />
+                </div>
+
+                <div className="mt-4 flex justify-center">
+                  <p className=" text-white font-bold text-md">{item.desc}</p>
+                </div>
               </div>
 
-              <div className="mt-2 md:mt-4 flex justify-center gap-2 flex-wrap">
-                <SplitText
-                  text="Construction Materials Partner"
-                  delay={40}
-                  animateBy="words"
-                  direction="top"
-                  className="text-3xl md:text-5xl font-bold text-[#125997]"
-                />
-              </div>
+              <div className="flex flex-col md:flex-row gap-3 md:gap-10 mt-8">
+                <Link to={item.path_1}>
+                  <button className="   cursor-pointer bg-[#0D47A1]  text-white px-6 py-3 rounded-lg font-semibold  hover:scale-105 hover:shadow-2xl transition-all ease-in">
+                    Get a free Quote
+                  </button>
+                </Link>
 
-              <div className="mt-4 flex justify-center">
-                <p className=" text-white font-bold text-md">
-                  Supplying high-quality raw materials for residential,
-                  commercial, and industrial projects.
-                </p>
-              </div>
-            </div>
-
-            <div className="flex flex-col md:flex-row gap-3 md:gap-10 mt-8">
-              <div className="">
-                <button
-                  onClick={handleClick}
-                  className="   cursor-pointer bg-[#0D47A1]  text-white px-6 py-3 rounded-lg font-semibold  hover:scale-105 hover:shadow-2xl transition-all ease-in"
-                >
-                  Get a free Quote
-                </button>
-              </div>
-
-              <Link to="/products">
-                <button className="relative overflow-hidden px-6 py-3 rounded-lg  border border-[#F2B715] bg-[#FFF9E6] text-[#263238] group  cursor-pointer ">
-                  <span
-                    className="absolute inset-0 bg-[#F2B715] scale-x-0 group-hover:scale-x-100 
+                <Link to={item.path_2}>
+                  <button className="relative overflow-hidden px-6 py-3 rounded-lg  border border-[#F2B715] bg-[#FFF9E6] text-[#263238] group  cursor-pointer ">
+                    <span
+                      className="absolute inset-0 bg-[#F2B715] scale-x-0 group-hover:scale-x-100 
                      transform origin-left transition-transform duration-300"
-                  ></span>
+                    ></span>
 
-                  <span className="relative flex gap-3 items-center group-hover:text-[#0D47A1] font-semibold transition-colors duration-300">
-                    Explore
-                    <ArrowRight className="transition-transform duration-300 group-hover:translate-x-1 group-hover:text-[#0D47A1]" />
-                  </span>
-                </button>
-              </Link>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <img
-            src="/images/hero_2.jpg"
-            alt=""
-            className="absolute inset-0 w-full h-full object-cover z-0"
-          />
-          <div className="absolute inset-0 bg-linear-to-t 
-                from-black/40 to-black/20"></div>
-          {/* Dark Overlay */}
-          {/* <div className="absolute inset-0 bg-black/40 z-10"></div> */}
-
-          {/* Content */}
-          <div className="relative z-20 flex flex-col items-center justify-center h-full text-white text-center px-4">
-            {/* <h1 className="text-2xl md:text-5xl font-bold">Take Control </h1>
-        <p className="mt-2 md:mt-4 text-2xl md:text-5xl font-bold">
-          of your <span className="text-[#00BCFF]">Water Usage</span>
-        </p> */}
-
-            <div className="mb-3 md:mb-5">
-              <div className="flex justify-center">
-                <BlurText
-                  text="Trusted Supplier of  "
-                  delay={70}
-                  animateBy="words"
-                  direction="top"
-                  className="text-3xl md:text-5xl font-bold "
-                />
-              </div>
-
-              <div className="mt-2 md:mt-4 flex justify-center gap-2 flex-wrap">
-                <SplitText
-                  text="Quality Construction Raw Materials"
-                  delay={70}
-                  animateBy="words"
-                  direction="top"
-                  className="text-3xl md:text-5xl font-bold text-[#125997]"
-                />
-              </div>
-
-              <div className="mt-4 flex justify-center">
-                <p className=" text-white font-bold text-md">
-                  "Strong Foundations Begin With the Right Materials."
-                </p>
+                    <span className="relative flex gap-3 items-center group-hover:text-[#0D47A1] font-semibold transition-colors duration-300">
+                      Explore
+                      <ArrowRight className="transition-transform duration-300 group-hover:translate-x-1 group-hover:text-[#0D47A1]" />
+                    </span>
+                  </button>
+                </Link>
               </div>
             </div>
-
-            <div className="flex flex-col md:flex-row md:gap-10 mt-8">
-              <div className="">
-                <button
-                  onClick={handleClick}
-                  className="   cursor-pointer bg-[#0D47A1]  text-white px-6 py-3 rounded-lg font-semibold  hover:scale-105 hover:shadow-2xl transition-all ease-in"
-                >
-                  Get a free Quote
-                </button>
-              </div>
-
-              <div>
-                <button className="relative overflow-hidden px-6 py-3 rounded-lg  border border-[#F2B715] bg-[#FFF9E6] text-[#263238] group  cursor-pointer ">
-                  <span
-                    className="absolute inset-0 bg-[#F2B715] scale-x-0 group-hover:scale-x-100 
-                               transform origin-left transition-transform duration-300"
-                  ></span>
-
-                  <span className="relative flex gap-3 items-center group-hover:text-[#0D47A1] font-semibold transition-colors duration-300">
-                    Explore
-                    <ArrowRight className="transition-transform duration-300 group-hover:translate-x-1 group-hover:text-[#0D47A1]" />
-                  </span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
--- src/pages/Cement.jsx
+++ src/pages/Cement.jsx
@@ -2,12 +2,97 @@ import React from 'react'
 import Navbar from '@/components/Navbar'
 import Banner from '@/components/Banner'
 import Footer from '@/components/Footer'
+import {motion} from "framer-motion"
+import BlurText from '@/components/BlurText'
 
 const Cement = () => {
   return (
     <>
     <Navbar/>
     <Banner/>
+
+    <section className="grid md:grid-cols-2 gap-10 max-w-7xl mx-5 md:mx-15 my-15 ">
+        <motion.div
+          initial={{ opacity: 0, x: -60 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7 }}
+          className="self-center "
+        >
+          <div className="flex justify-center  md:hidden">
+            <BlurText
+              text="About"
+              delay={70}
+              animateBy="words"
+              direction="top"
+              className="text-5xl font-bold  text-center mb-5 text-[#0D47A1]"
+            />
+          </div>
+          <img
+            src="/images/abt.avif"
+            alt=""
+            className="w-full rounded-lg hover:shadow-2xl"
+          />
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: 60 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          <div className=" hidden  md:block">
+            <BlurText
+              text="About"
+              delay={70}
+              animateBy="words"
+              direction="top"
+              className="text-5xl font-bold  text-center mb-5 text-[#0D47A1]"
+            />
+          </div>
+
+          <div className="mt-5 space-y-3 ">
+            <p className="text-gray-900 font-medium text-lg text-justify">
+              We are a trusted supplier of construction raw materials, committed
+              to delivering high-quality building materials that form the
+              backbone of strong and long-lasting structures.
+            </p>
+
+            <p className="text-gray-900 font-medium text-lg text-justify">
+              With years of experience in the construction supply industry, we
+              specialize in providing cement, sand, bricks, steel rods,
+              aggregates, and other essential materials to contractors,
+              builders, developers, and individual customers.
+            </p>
+
+            <p className="text-gray-900 font-medium text-lg text-justify">
+              We understand that construction timelines are critical. That’s why
+              we ensure on-time delivery, consistent quality, and transparent
+              business practices—helping our clients complete projects without
+              delays or compromises.
+            </p>
+
+            {/* <div className="mt-5 space-y-3 flex flex-col items-center md:flex-row md:items-start gap-3">
+              <div className=" group flex gap-2">
+                <CircleCheckBig className="text-[#edb928] group-hover:text-[#0D47A1]" />
+                <p className="font-bold text-md text-[#0D47A1] group-hover:text-[#edb928] group-hover:cursor-pointer transition-all">
+                  Quality materials
+                </p>
+              </div>
+              <div className=" group flex gap-2">
+                <CircleCheckBig className="text-[#edb928] group-hover:text-[#0D47A1]" />
+                <p className="font-bold text-md text-[#0D47A1] group-hover:text-[#edb928] group-hover:cursor-pointer transition-all">
+                  Reliable supply
+                </p>
+              </div>
+              <div className=" group flex gap-2">
+                <CircleCheckBig className="text-[#edb928] group-hover:text-[#0D47A1]" />
+                <p className="font-bold text-md text-[#0D47A1] group-hover:text-[#edb928] group-hover:cursor-pointer transition-all">
+                  Honest pricing
+                </p>
+              </div>
+            </div> */}
+          </div>
+        </motion.div>
+      </section>
     <Footer/>
     </>
   )
